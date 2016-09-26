4d3b4c5a0fb88afe6e451079e940aae2d8889aa8
Can Elmas <Can.Elmas@monitise.com>
2016-09-26 12:21:52 +0300

    Ask project name as parameter

--- summarize.py
+++ summarize.py
@@ -10,8 +10,8 @@ from jenkins_info import JenkinsInfo
 
 
 def summarize():
-    if len(sys.argv) > 2:
-        for build_variant in sys.argv[2:]:
+    if len(sys.argv) > 3:
+        for build_variant in sys.argv[3:]:
 
             summary = {}
 
@@ -19,13 +19,14 @@ def summarize():
             summary.update(BuildInfo.fetch())
 
             # Apk Info
-            summary.update(ApkInfo.fetch(sys.argv[1], build_variant))
+            summary.update(ApkInfo.fetch(sys.argv[2], build_variant))
 
             # Jenkins info
             summary.update(JenkinsInfo.fetch())
 
-            # Date
-            summary.update({'created': str(datetime.now())})
+            # Date & Project Name
+            summary.update({'created': str(datetime.now()),
+                            'name': sys.argv[1]})
 
             # print summary
             print json.dumps(summary, indent=4)
@@ -34,15 +35,13 @@ def summarize():
             requests.put('http://localhost:3000/api/v1/projects',
                          data=json.dumps(summary),
                          headers={'Content-Type': 'application/json;charset=UTF-8'})
-
-
     else:
         help()
 
 
 def help():
-    print "usage : ./summarize.py <main_module_name> <build_variant_1>...<build_variant_n>\n"
-    print "Ex: ./summarize.py app prod-release lite-release local-debug\n"
+    print "usage : ./summarize.py <project_name> <main_module_name> <build_variant_1>...<build_variant_n>\n"
+    print "Ex: ./summarize.py lovely-app-android app prod-release lite-release local-debug\n"
 
 
 if __name__ == '__main__':
